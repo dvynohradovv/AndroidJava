b8a8b2c0c64998592be17ebc63b1d54983a046e1
Dmytro Vynohradov <dimarkens@gmail.com>
2022-05-29 22:51:12 +0300

    Lab4 finish

---
+++ 4lab/app/src/main/java/com/android/a4lab/MainActivity.java
@@ -0,0 +1,18 @@
+package com.android.a4lab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
